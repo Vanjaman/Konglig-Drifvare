cef2ab76e7ae51b4109897991b42b30206295dca
juliuscaesar1597 <abhinav.sasikumar@gmail.com>
2021-10-22 14:12:06 +0200

    Fixade avrundningen till heltal

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def ping(ctx):
         title="Pong!",
         description=f"""Drifveriet svarar vanligtvis snabbare än ljusets hastighet,
          men för att ~~nØllans~~ ettans synapser skall hänga med
-         svarar vi på {round(bot.latency*1000,4)} ms"""
+         svarar vi på {round(bot.latency*1000)} ms"""
     )
     embed.set_thumbnail(
         url="https://cdn.discordapp.com/attachments/572159063505371137/900800963651199086/drifveriet.png")
