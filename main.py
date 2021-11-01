4a1b7f01dbccff6d17ef1c4f60aade3aa25a5999
Vanja Grigoriev <ivang@kth.se>
2021-11-01 23:52:49 +0100

    Stopped bot from usinghelper_functions.jubla in course channels.

--- main.py
+++ main.py
@@ -76,8 +76,9 @@ async def on_message(message):
              )
         )
 
-    elif random.randint(0, 500) == 420:
-        await helper_functions.jubla(message)
+    elif random.randint(0, 420) == 420:
+        if message.channel.category not in ("═════ Kurskanaler ═════", "═══ \"Avslutade\" Kurser  ══"):
+            await helper_functions.jubla(message)
 
     await bot.process_commands(message)
 
